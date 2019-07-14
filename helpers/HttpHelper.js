aa323111493c0b05b073090e5832270d52a31b85
Ley Gabriel <gabriel.ley@outlook.fr>
2019-07-14 13:15:21 +0200

    Http helper

--- helpers/HttpHelper.js
+++ helpers/HttpHelper.js
@@ -1,11 +1,75 @@
 import axios from "axios";
 
 class Httphelper{
-    constructor(){
+    httpClient;
 
+    constructor(){
+        this.httpClient = axios.create({
+            baseURL: "izy/",
+            headers: {"Content-Type": "application/json"}
+        });
     }
 
-    request(method, ){
+    httpRequest(method, url, params, handleResponse = this.defaultHandler){
+        switch(method){
+            case 'get':
+                return this.get(url, params, handleResponse);
+            case 'post':
+                return this.post(url, params, handleResponse);
+            case 'put':
+                return this.put(url, params, handleResponse);
+            case 'delete':
+                return this.delete(url, params, handleResponse);
+            default:
+                throw "invalid method";
+        }
+    }
 
+    get(url, params, handleResponse){
+        this.httpClient.get(url, {
+            params: params
+        })
+        .then(handleResponse)
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    post(url, params, handleResponse){
+        this.httpClient.post(url, {
+            params: params
+        })
+        .then(handleResponse)
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    put(url, params, handleResponse){
+        this.httpClient.post(url, {
+            params: params
+        })
+        .then(handleResponse)
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    delete(url, params, handleResponse){
+        this.httpClient.post(url, {
+            params: params
+        })
+        .then(handleResponse)
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    defaultHandler(response){
+        if(response.status >= 300){
+            throw response.statusText;
+        } else {
+            return response.data;
+        }
     }
 }
